22d010217765fa74b1adf4ac5180d92ea06e0db7
recalcitrantsupplant <dcchabgood@gmail.com>
2025-08-22 15:46:20 +1000

    set non root user and copy config to opt. update to recommended command

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,9 +1,17 @@
 #!/bin/sh
 
+# Create configuration directory if it doesn't exist
+mkdir -p "${FUSEKI_BASE}/configuration"
+
+# Copy any configs from /opt/fuseki/configuration to runtime location
+if [ -d "/opt/fuseki/configuration" ]; then
+    cp -r /opt/fuseki/configuration/* "${FUSEKI_BASE}/configuration/" 2>/dev/null || true
+fi
+
 exec \
   "${JAVA_HOME}/bin/java" \
   ${JAVA_OPTS} \
   -Xshare:off \
   -Dlog4j.configurationFile="${FUSEKI_HOME}/log4j2.properties" \
   -cp "${FUSEKI_HOME}/fuseki-server.jar:${FUSEKI_HOME}/lib/*" \
-  org.apache.jena.fuseki.cmd.FusekiCmd
+  org.apache.jena.fuseki.main.cmds.FusekiServerCmd
